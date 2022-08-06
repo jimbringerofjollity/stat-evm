a56bf895186811ee655d278c2e20bf7a9309988f
Jim Tao <myemailaint@gmail.com>
2022-08-06 11:19:22 -0400

    Add files via upload
    fixed fit-test

--- precompile/fit.go
+++ precompile/fit.go
@@ -61,77 +61,72 @@ func MakeFitRetArgs() abi.Arguments {
 	}
 }
 
-func createFitPrecompile(precompileAddr common.Address) StatefulPrecompiledContract {
-	f := func(
-		evm PrecompileAccessibleState,
-		callerAddr common.Address,
-		addr common.Address,
-		input []byte,
-		suppliedGas uint64,
-		readOnly bool,
-	) (retb []byte, remainingGas uint64, err error) {
-		fmt.Println("input", hexutil.Encode(input))
-		inputCopy := make([]byte, len(input))
-		copy(inputCopy, input)
-
-		var errb [32]byte
-		errb[31] = 0xaa
-
-		valsI, err := MakeFitArgs().UnpackValues(inputCopy)
-		if err != nil {
-			return errb[:], suppliedGas, err
-		}
-
-		vals, ok := valsI[0].([]*big.Int)
-		if !ok {
-			return errb[:], suppliedGas, errors.New("invalid type")
-		}
-		if len(vals)%2 != 0 {
-			return errb[:], suppliedGas, errors.New("invalid parity of len(vals)")
-		}
-
-		xys3 := big.NewInt(0)
-		for j := 0; j < len(vals); j += 2 {
-			addend := big.NewInt(0)
-			addend.Mul(vals[j], vals[j+1])
-			xys3.Add(xys3, addend)
-		}
-		xys3.Mul(xys3, big.NewInt(3))
-		xsys := big.NewInt(0)
-		xs := big.NewInt(0)
-		ys := big.NewInt(0)
-		for j := 0; j < len(vals); j += 2 {
-			xs.Add(xs, vals[j])
-			ys.Add(ys, vals[j+1])
-		}
-		xsys.Mul(xs, ys)
-		num := big.NewInt(0)
-		num.Sub(xys3, xsys)
-		denom := big.NewInt(0)
-		x2s3 := big.NewInt(0)
-		for j := 0; j < len(vals); j += 2 {
-			addend := big.NewInt(0)
-			addend.Mul(vals[j], vals[j])
-			x2s3.Add(x2s3, addend)
-		}
-		x2s3.Mul(x2s3, big.NewInt(3))
-		xs2 := big.NewInt(0)
-		xs2.Mul(xs, xs)
-		denom.Sub(x2s3, xs2)
-		perc := big.NewInt(0)
-		perc.Mul(num, big.NewInt(100))
-		perc.Div(perc, denom)
-		// fitb := 100 * (3*(v1*v2+v3*v4+v5*v6) - (v1+v3+v5)*(v2+v4+v6)) / (3*(v1*v1+v3*v3+v5*v5) - (v1+v3+v5)*(v1+v3+v5))
-		// fita := 100*(v2+v4+v6)/3 - fitb*(v1+v3+v5)/3
-		retb, err = MakeFitRetArgs().PackValues([]interface{}{perc})
-		if err != nil {
-			return errb[:], suppliedGas, err
-		}
-
-		return retb, suppliedGas, nil
+func fit(
+	evm PrecompileAccessibleState,
+	callerAddr common.Address,
+	addr common.Address,
+	input []byte,
+	suppliedGas uint64,
+	readOnly bool,
+) (retb []byte, remainingGas uint64, err error) {
+	fmt.Println("input", hexutil.Encode(input))
+	inputCopy := make([]byte, len(input))
+	copy(inputCopy, input)
+	valsI, err := MakeFitArgs().UnpackValues(inputCopy)
+	if err != nil {
+		return nil, suppliedGas, err
 	}
 
-	funcGetFit := newStatefulPrecompileFunction(fitSignature, f)
+	vals, ok := valsI[0].([]*big.Int)
+	if !ok {
+		return nil, suppliedGas, errors.New("invalid type")
+	}
+	if len(vals)%2 != 0 {
+		return nil, suppliedGas, errors.New("invalid parity of len(vals)")
+	}
+
+	xys3 := big.NewInt(0)
+	for j := 0; j < len(vals); j += 2 {
+		addend := big.NewInt(0)
+		addend.Mul(vals[j], vals[j+1])
+		xys3.Add(xys3, addend)
+	}
+	xys3.Mul(xys3, big.NewInt(3))
+	xsys := big.NewInt(0)
+	xs := big.NewInt(0)
+	ys := big.NewInt(0)
+	for j := 0; j < len(vals); j += 2 {
+		xs.Add(xs, vals[j])
+		ys.Add(ys, vals[j+1])
+	}
+	xsys.Mul(xs, ys)
+	num := big.NewInt(0)
+	num.Sub(xys3, xsys)
+	denom := big.NewInt(0)
+	x2s3 := big.NewInt(0)
+	for j := 0; j < len(vals); j += 2 {
+		addend := big.NewInt(0)
+		addend.Mul(vals[j], vals[j])
+		x2s3.Add(x2s3, addend)
+	}
+	x2s3.Mul(x2s3, big.NewInt(3))
+	xs2 := big.NewInt(0)
+	xs2.Mul(xs, xs)
+	denom.Sub(x2s3, xs2)
+	perc := big.NewInt(0)
+	perc.Mul(num, big.NewInt(100))
+	perc.Div(perc, denom)
+	// fitb := 100 * (3*(v1*v2+v3*v4+v5*v6) - (v1+v3+v5)*(v2+v4+v6)) / (3*(v1*v1+v3*v3+v5*v5) - (v1+v3+v5)*(v1+v3+v5))
+	// fita := 100*(v2+v4+v6)/3 - fitb*(v1+v3+v5)/3
+	retb, err = MakeFitRetArgs().PackValues([]interface{}{perc})
+	if err != nil {
+		return nil, suppliedGas, err
+	}
+
+	return retb, suppliedGas, nil
+}
+
+func createFitPrecompile(precompileAddr common.Address) StatefulPrecompiledContract {
 	// Return the contract with no fallback function.
-	return newStatefulPrecompileWithFunctionSelectors(nil, []*statefulPrecompileFunction{funcGetFit})
+	return newStatefulPrecompileWithFunctionSelectors(nil, []*statefulPrecompileFunction{newStatefulPrecompileFunction(fitSignature, fit)})
 }
--- precompile/fit_test.go
+++ precompile/fit_test.go
@@ -4,15 +4,17 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMedian(t *testing.T) {
-	a, err := MakeFitArgs().PackValues([]interface{}{big.NewInt(10), big.NewInt(2), big.NewInt(3)})
+func TestFit(t *testing.T) {
+	a, err := MakeFitArgs().PackValues([]interface{}{[]*big.Int{big.NewInt(1), big.NewInt(1), big.NewInt(2), big.NewInt(3), big.NewInt(3), big.NewInt(2)}})
 	require.NoError(t, err)
-	t.Log(hexutil.Encode(a[:]))
-	vls, err := MakeFitArgs().UnpackValues(a)
+	//t.Log(hexutil.Encode(a[:]))
+	res, _, err := fit(nil, common.Address{}, common.Address{}, a, 0, false)
+	require.NoError(t, err)
+	vls, err := MakeFitRetArgs().UnpackValues(res)
 	require.NoError(t, err)
 	t.Log(vls)
 }
